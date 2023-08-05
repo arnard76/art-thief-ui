91e9f9b48fa0e18ce35c2df681544a62ac5dd80a
Arnav Shekaran <78939786+arnard76@users.noreply.github.com>
2023-08-05 21:51:28 +1200

    a cleaner working drawing canvas component
    replaced old one with
    https://svelte.dev/repl/434e0b14546747688401e8808c060a23?version=3.47.0

--- src/routes/drawing/+page.svelte
+++ src/routes/drawing/+page.svelte
@@ -3,13 +3,16 @@
 
   import DrawingCanvas from "$lib/DrawingCanvas.svelte";
 
-  export let drawingCanvas;
-  $: console.log(drawingCanvas);
+  export let drawingCanvas = undefined;
+
+  function getSketchedImage() {
+    return drawingCanvas?.toDataURL("image/png");
+  }
 
   $: img = drawingCanvas?.toDataURL("image/png");
   $: console.log(img);
 </script>
 
-<DrawingCanvas bind:drawingCanvas />
+<DrawingCanvas bind:canvas={drawingCanvas} width={640} height={512} />
 
 <button />
