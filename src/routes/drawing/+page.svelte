f6c9a1f718550543c569968f0b5aad2d8f57f0fc
Arnav Shekaran <78939786+arnard76@users.noreply.github.com>
2023-08-06 10:01:22 +1200

    attempt to request API

--- src/routes/drawing/+page.svelte
+++ src/routes/drawing/+page.svelte
@@ -6,13 +6,47 @@
   export let drawingCanvas = undefined;
 
   function getSketchedImage() {
-    return drawingCanvas?.toDataURL("image/png");
+    return new File(
+      [new Blob([drawingCanvas.toDataURL("image/png")], { type: "image/png" })],
+      "sketch.png"
+    );
+    // return new Blob([drawingCanvas?.toDataURL("image/png")], {
+    //   type: "image/png",
+    // });
   }
 
-  $: img = drawingCanvas?.toDataURL("image/png");
-  $: console.log(img);
+  async function generateForgery() {
+    const res = await fetch(
+      "http://localhost:3001/?" +
+        new URLSearchParams({
+          prompt: "hello",
+        }),
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "image/png",
+        },
+        body: getSketchedImage(),
+      }
+    );
+
+    console.log(res.request);
+    console.log("Image uploaded successfully!");
+  }
 </script>
 
+<!-- Art being stolen 🖼️ -->
+
+<!-- Initial Sketch -->
 <DrawingCanvas bind:canvas={drawingCanvas} width={640} height={512} />
 
-<button />
+<button
+  on:click={async () =>
+    console.log(
+      getSketchedImage(),
+      typeof getSketchedImage(),
+      await generateForgery()
+    )}>Generate image</button
+>
+
+<!-- Art Forgery (generated image) -->
