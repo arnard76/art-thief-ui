7e942ac3a354cad7f4d68735d7d222338842c7be
Arnav Shekaran <78939786+arnard76@users.noreply.github.com>
2023-08-05 16:03:40 +1200

    deleting more unrelated stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,12 @@
 <script>
-  import Counter from "./Counter.svelte";
 </script>
 
 <svelte:head>
-  <title>Home</title>
-  <meta name="description" content="Svelte demo app" />
+  <title>The Art Thief</title>
+  <meta name="description" content="The Art Thief" />
 </svelte:head>
 
-<section>
-  <Counter />
-</section>
+<section />
 
 <style>
-  section {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    flex: 0.6;
-  }
-
-  h1 {
-    width: 100%;
-  }
-
-  .welcome {
-    display: block;
-    position: relative;
-    width: 100%;
-    height: 0;
-    padding: 0 0 calc(100% * 495 / 2048) 0;
-  }
-
-  .welcome img {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    display: block;
-  }
 </style>
