9574213b636d6b53b800272339658163b173c91f
Raymond's MacBook Pro <raymond16@hotmail.co.nz>
2023-08-06 10:26:43 +1200

    Add typewriter effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,76 +1,106 @@
 <script>
-  import Button from "$lib/Button.svelte";
-  import "./styles.css";
+    import Typewriter from 'svelte-typewriter';
+    import Button from '$lib/Button.svelte';
+    import './styles.css';
 
-  let startBtn;
-  let showBox = false;
+    let startBtn;
+    let showStartBtn = true;
+    let showBox = false;
+    let keepCursorOnFinish = true;
+    let interval = 75;
 
-  const handleClick = () => {
-    showBox = true;
-  };
+    const handleClick = () => {
+        showBox = true;
+        showStartBtn = false;
+    };
 </script>
 
 <svelte:head>
-  <title>The Art Thief</title>
-  <meta name="description" content="The Art Thief" />
+    <title>The Art Thief</title>
+    <meta name="description" content="The Art Thief" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
+        rel="stylesheet"
+    />
 </svelte:head>
 
-<div class="button-container" bind:this={startBtn} on:click={handleClick}>
-  <Button>Start</Button>
-</div>
+{#if showStartBtn}
+    <div class="button-container" bind:this={startBtn} on:click={handleClick}>
+        <Button>Start</Button>
+    </div>
+{/if}
 
 {#if showBox}
-  <div class="button-container">
-    <div class="box"></div>
-  </div>
+    <div class="box-container">
+        <div class="box">
+            <Typewriter {interval} {keepCursorOnFinish}>
+                <p class="mission-statement">
+                    You are an art thief... 
+                    Your mission is to steal a historic piece of artwork and replace it with a forgery. 
+                    Should you successfully steal the piece of artwork, you will have successfully rewritten history...
+                </p>
+            </Typewriter>
+        </div>
+    </div>
 {/if}
 
 <div class="background">
-  <img src="./gameArt/landingPage.svg" alt="" srcset="" />
-  <div class="black" />
+    <img src="./gameArt/landingPage.svg" alt="" srcset="" />
 </div>
 
 <style>
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Press Start 2P', Helvetica, sans-serif;
+    }
 
-  .box {
-    width: 50%;
-    height: 600px;
-    background-color: rgba(91, 91, 91, 0.718);
-  }
+    .box-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
 
-  .container {
-    padding: 20px;
-  }
+    .mission-statement {
+        font-size: 33px;
+    }
 
-  .button-container {
-    display: flex;
-    justify-content: center;
-    padding: 16px 0;
-  }
+    .box-container .box {
+        width: 50%;
+        height: 600px;
+        background-color: rgba(91, 91, 91, 0.718);
+        padding: 8px;
+    }
 
-.background {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100vh;
-  z-index: -10;
-  /* Remove display and justify-content properties */
+    .container {
+        padding: 20px;
+    }
 
-  /* Add this to center the image horizontally and vertically */
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
+    .button-container {
+        display: flex;
+        justify-content: center;
+        padding: 16px 0;
+    }
 
-.background > img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
+    .background {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: -10;
+        display: flex;
+        align-items: center;
+        justify-content: center; /* Center both horizontally and vertically */
+    }
+
+    .background > img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
 </style>
