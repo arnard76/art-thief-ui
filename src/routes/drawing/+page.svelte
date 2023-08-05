29fd40c5aab286fc528d32fb6c0ef2423e0dcaf9
Arnav Shekaran <78939786+arnard76@users.noreply.github.com>
2023-08-05 19:32:48 +1200

    page to display drawing canvas + some other stuff :)

---
+++ src/routes/drawing/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  // @ts-nocheck
+
+  import DrawingCanvas from "$lib/DrawingCanvas.svelte";
+
+  export let drawingCanvas;
+  $: console.log(drawingCanvas);
+
+  $: img = drawingCanvas?.toDataURL("image/png");
+  $: console.log(img);
+</script>
+
+<DrawingCanvas bind:drawingCanvas />
+
+<button />
