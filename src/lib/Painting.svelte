f976fa54f82a4b7daf7b23bfc1418133e47a1651
rl16432 <rluo52237@gmail.com>
2023-08-06 14:29:32 +1200

    add painting frames

---
+++ src/lib/Painting.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let isVertical = false;
+  export let src = '';
+</script>
+
+<div class={isVertical ? 'frame' : 'frame-vert'}>
+  <img
+    class="artwork"
+    src={src}
+    alt="artwork"
+  />
+</div>
+
+<style>
+  .frame {
+    background-image: url("/frame.jpg");
+    background-size: cover;
+    background-position: center;
+    padding: 50px;
+  }
+
+  .frame-vert {
+    background-image: url("/frame_hor.jpg");
+    background-size: cover;
+    background-position: center;
+    padding: 50px;
+  }
+
+  .artwork {
+    max-height: 512px;
+    max-width: 640px;
+  }
+</style>
